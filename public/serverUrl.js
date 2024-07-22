3d4c0a949ea4aae66373e19445b10aa3d5960ed5
klu0926 <lukuoyu@gmail.com>
2024-07-22 16:24:06 +0800

    add heroku url to serverUrl.js

--- public/serverUrl.js
+++ public/serverUrl.js
@@ -1,5 +1,5 @@
 const isLocal = window.location.hostname === 'localhost';
-const heroku = 'heroku url here'
+const heroku = 'https://comment-picker-2755d12b5505.herokuapp.com'
 const local = 'http://localhost:3000'
 
 const server = isLocal ? local : heroku
